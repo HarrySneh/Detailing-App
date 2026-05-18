ea3ba82f3440e30099da4c8bab92a1ca5b2f7109
Yunus <hmckaville@gmail.com>
2026-05-19 02:00:00 +0500

    1.0.2.3

--- iron-amber/src/App.tsx
+++ iron-amber/src/App.tsx
@@ -1,12 +1,14 @@
-import { Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Terms from "./pages/Terms";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/terms" element={<Terms />} />
-    </Routes>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/terms" element={<Terms />} />
+      </Routes>
+    </HashRouter>
   );
 }
